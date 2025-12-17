9496f7213166e79b04f2a96d6dfed54a96d551ad
daoanhkhoa123 <158530319+daoanhkhoa123@users.noreply.github.com>
2025-12-17 20:06:38 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   .env
    	modified:   runtime/sample.ipynb
    	modified:   runtime/ultils.py

--- runtime/ultils.py
+++ runtime/ultils.py
@@ -4,9 +4,9 @@ from typing import Callable
 
 def getpass_deco(message:str=""):
     def wrapper(fn:Callable):
-        def fn2():
-            arg = getpass.getpass(message)
-            return fn(arg)
+        def fn2(*args, **kwargs):
+            secret = getpass.getpass(message)
+            return fn(secret, *args, **kwargs)
         return fn2
         
     return wrapper
@@ -20,3 +20,14 @@ def gdown_folder(link:str):
     except subprocess.CalledProcessError as e:
         print(f"Download failed: {e}")
         return None
+    
+@getpass_deco("Type in the ngrok token:")
+def ngrok_start(token:str, port:int=5000):
+    try:
+        subprocess.run(["ngrok", "config", "add-authtoken", token], check=True)
+        subprocess.run(["ngrok", "http", str(port)], check=True)
+    
+    except subprocess.CalledProcessError as e:
+        print("Ngrok failed:" , e)
+
+        
